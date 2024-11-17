be040ef63f71462c3be77daf173a3714cc45af64
d1savow3d <35790653+thisisu@users.noreply.github.com>
2024-11-17 10:06:21 -0600

    Update PrivWindoze.bat

--- PrivWindoze.bat
+++ PrivWindoze.bat
@@ -1,8 +1,8 @@
 :: PrivWindoze
 :: Created by Furtivex
 @echo OFF && color 17
-title PrivWindoze by Furtivex - Version 2.6.8
-ECHO(PrivWindoze by Furtivex - Version 2.6.8
+title PrivWindoze by Furtivex - Version 2.6.9
+ECHO(PrivWindoze by Furtivex - Version 2.6.9
 ECHO.
 ECHO.
 REM ~~~~~~~~~~~~~~~~~~~~~~~~>
@@ -849,7 +849,7 @@ FOR %%g in (
 
 Echo(~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>"%TEMP%\pwindoze.txt"
 Echo(PrivWindoze by Furtivex>>"%TEMP%\pwindoze.txt"
-Echo(Version: 2.6.8 ^(11.17.2024^)>>"%TEMP%\pwindoze.txt"
+Echo(Version: 2.6.9 ^(11.17.2024^)>>"%TEMP%\pwindoze.txt"
 Echo(Operating System: %OS% %ARCH%>>"%TEMP%\pwindoze.txt"
 Echo(Ran by "%username%" ^("%COMPUTERNAME%"^) ^(%USERSTATUS%^) on %StartDate% at %StartTime%>>"%TEMP%\pwindoze.txt"
 Echo(~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>"%TEMP%\pwindoze.txt"
